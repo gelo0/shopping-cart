84b8765f954d9c28767403b2b079b739fd10f83a
Gelo <gelo0@o2.pl>
2022-07-14 13:16:20 +0200

    First commit

---
+++ src/components/StoreItem.js
@@ -0,0 +1,43 @@
+import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+export function StoreItem(item) {
+  const {
+    getItemQuantity, 
+    increaseCartQuantity, 
+    decreaseCartQuantity, 
+    removeFromCart,
+  } = useShoppingCart()
+  const quantity = getItemQuantity(item.id)
+  
+  return (
+    <Card className="h-100">
+      <Card.Img 
+        variant="top" 
+        src={item.imgUrl} 
+        height="200px" 
+        style={{ objectFit: "cover"}}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{item.name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(item.price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(item.id) }>+ Add to Card</Button>
+          ) : <div className="d-flex align-items-center flex-column" style={{gap:".5rem"}}>
+            <div className="d-flex align-items-center justify-content-center" style={{gap:".5rem"}}>
+              <Button onClick={() => decreaseCartQuantity(item.id) }>-</Button>
+              <div>
+              <span className="fs-3">{quantity}</span> in cart
+              </div>
+              <Button onClick={() => increaseCartQuantity(item.id) }>+</Button>
+            </div>
+            <Button variant="danger" size="sm" onClick={() => removeFromCart(item.id) }>Remove</Button>
+            </div>}
+        </div>
+      </Card.Body>
+    </Card>
+)}
